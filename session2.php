ff91f6765765c6ddf4dc921f39cea3f7eeddce43
harrycc8 <161590306+harrycc8@users.noreply.github.com>
2024-03-05 17:22:19 +0000

    Add files via upload

--- session2.php
+++ session2.php
@@ -1,1 +1,6 @@
-
+<?php
+session_start();
+#use echo statements to output the values stored in $_SESSION
+echo '<BR>Food: ' . $_SESSION['myFavFood'];
+echo '<BR>Drink: ' . $_SESSION['myFavDrink'];
+echo 'Color: ' . $_SESSION['myFavColor'];
